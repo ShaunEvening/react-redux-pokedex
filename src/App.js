b2b30124e2d2b3955c1c87c9fcd936a83626e6ed
shaun lloyd <shaunlloydmedia@gmail.com>
2018-10-02 20:13:11 -0400

    Move search bar to component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SearchForm } from './SearchForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <input type="text" placeholder="pokemon number" />
-          <button>Search</button>
+          <SearchForm />
         </header>
       </div>
     );
